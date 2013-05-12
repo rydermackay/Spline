dc17811ed4a7d68e173411bcab9c9b8a9c9f5688
Ryder Mackay <ryder.mackay@gmail.com>
2013-05-11 21:45:58 -0400

    Initial Commit

---
+++ Spline/RGMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RGMAppDelegate.h
+//  Spline
+//
+//  Created by Ryder Mackay on 2013-05-11.
+//  Copyright (c) 2013 Ryder Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spline/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Spline
+//
+//  Created by Ryder Mackay on 2013-05-11.
+//  Copyright (c) 2013 Ryder Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RGMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RGMAppDelegate class]));
+    }
+}
